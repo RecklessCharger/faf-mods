324ca5659d9c1ecf4ddba65b116bc2065e5a418f
Thomas Young <thomas@upcoder.com>
2017-06-01 20:46:01 +0200

    ok, so this update method (based on the additional camera stuff mod update) is much mess jerky

---
+++ RenderCircleAtCursor/hook/lua/ui/controls/worldview.lua
@@ -0,0 +1,31 @@
+--local _cursorUpdateFunctions = {}
+
+--function AddCursorUpdateFunction(fn)
+--    table.insert(_cursorUpdateFunctions, fn)
+--end
+
+local ring = nil
+
+local oldWorldView = WorldView 
+WorldView = Class(oldWorldView, Control) {
+    HandleEvent = function(self, event)
+        return oldWorldView.HandleEvent(self, event)
+    end,
+
+    OnUpdateCursor = function(self)
+--        for i,v in _cursorUpdateFunctions do
+--            if v then v() end
+--        end
+        local texture = '/mods/RenderCircleAtCursor/textures/range_ring.dds'
+        local radius = 7
+        if ring == nil then
+            local Decal = import('/lua/user/userdecal.lua').UserDecal
+            ring = Decal(GetFrame(0))
+            ring:SetTexture(texture)
+            ring:SetScale({math.floor(2.03*radius), 0, math.floor(2.03*radius)})
+        end
+        ring:SetPosition(GetMouseWorldPos())
+
+        return oldWorldView.OnUpdateCursor(self)
+    end,
+}
--- RenderCircleAtCursor/hook/lua/ui/game/gamemain.lua
+++
@@ -1,8 +0,0 @@
-local originalCreateUI = CreateUI 
-local modFolder = 'RenderCircleAtCursor'
-local BeatFunction = import('/mods/' .. modFolder .. '/modules/beat_function.lua').BeatFunction
-
-function CreateUI(isReplay) 
-  originalCreateUI(isReplay) 
-  AddBeatFunction(BeatFunction)
-end
