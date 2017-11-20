a1426fcf298c2d4aec2f400456ed91054da70cc1
Thomas Young <thomas@upcoder.com>
2017-11-20 09:04:13 +0100

    added more info to UnitInfoToLog

--- UnitInfoToLog/hook/lua/ui/game/gamemain.lua
+++ UnitInfoToLog/hook/lua/ui/game/gamemain.lua
@@ -1,5 +1,38 @@
 local oldId = nil
 
+function LogInfoFor(e)
+    LOG("Unit selected with ID: "..e:GetEntityId())
+    LOG("GetEconData() returns:"..repr(e:GetEconData()))
+    LOG("GetCommandQueue() returns:"..repr(e:GetCommandQueue()))
+    --LOG("GetGuardedEntity() returns:"..repr(e:GetGuardedEntity())) -- looks like this causes a hard crash, when there is one!
+    if e:GetGuardedEntity() then
+        LOG("Has a guarded entity")
+    end
+    if e:GetCreator() then
+        LOG("Has a creator")
+    end
+    Units = import('/mods/common/units.lua')
+    if Units.being_built[e:GetEntityId()] then
+        if Units.being_built[e:GetEntityId()] == e then
+            LOG("Being built")
+        else
+            LOG("Units.being_built is not consistent")
+        end
+    end
+    LOG("GetFootPrintSize() returns:"..repr(e:GetFootPrintSize()))
+    LOG("GetBuildRate() returns:"..repr(e:GetBuildRate()))
+    LOG("GetWorkProgress() returns:"..repr(e:GetWorkProgress()))
+    LOG("GetSelectionSets() returns:"..repr(e:GetSelectionSets()))
+    -- following reslts in loads of output
+    --LOG("GetBlueprint() returns:"..repr(e:GetBlueprint()))
+    LOG("Categories:"..repr(e:GetBlueprint().Categories))
+    LOG("IsIdle() returns:"..repr(e:IsIdle()))  
+    LOG("IsAutoMode() returns:"..repr(e:IsAutoMode()))  
+    LOG("IsInCategory('TECH1') returns:"..repr(e:IsInCategory('TECH1')))
+    --LOG("GetWorkProgess() returns:"..repr(e:GetWorkProgess())) -- generates error: attempt to call method `GetWorkProgess' (a nil value)
+    LOG("================")
+end
+
 function MyBeatFunction()
     local t = GetSelectedUnits()
     if not t or not t[1] then
@@ -7,14 +40,20 @@ function MyBeatFunction()
         return
     end
     local e = t[1]
+    --if e:GetWorkProgress() > 0 then
+    --    LOG("work progress:"..repr(e:GetWorkProgress()))
+    --end
     if e:GetEntityId() == oldId then return end
     oldId = e:GetEntityId()
-    LOG("Unit selected with ID: "..e:GetEntityId())
-    LOG("GetEconData() returns:"..repr(e:GetEconData()))
-    LOG("GetCommandQueue() returns:"..repr(e:GetCommandQueue()))
-    LOG("GetGuardedEntity() returns:"..repr(e:GetGuardedEntity()))
-    LOG("IsIdle() returns:"..repr(e:IsIdle()))  
-    LOG("IsInCategory('TECH1') returns:"..repr(e:IsInCategory('TECH1')))  
+    LOG("================")
+    LOG("Game tick = "..GameTick())
+    LOG("Selected unit:")
+    LogInfoFor(e)
+    local focus = e:GetFocus()
+    if focus and not focus:IsDead() then
+        LOG("Focus:")
+        LogInfoFor(focus)
+    end
 end
 
 local originalCreateUI = CreateUI 
