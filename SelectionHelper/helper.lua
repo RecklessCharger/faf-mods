101eb09ca19c9cff6fdd119bf60e2add29a8f5a8
Thomas Young <thomas@upcoder.com>
2019-03-02 17:25:07 +0100

    renamed helper methods, added FilterOrSelect methods, added SelectFactoryWithNearestRallyPoint

--- SelectionHelper/helper.lua
+++ SelectionHelper/helper.lua
@@ -1,15 +1,10 @@
--- to bind this as a hotkey in your game.prefs make an action like this:
--- UI_Lua import("/mods/AddNearestByCategoryToSelection/lua.lua").AddNearestByCategoryToSelection("+idle TECH1 ENGINEER")
--- UI_Lua import("/mods/AddNearestByCategoryToSelection/lua.lua").AddNearestByCategoryToSelection("+idle +excludeengineers MOBILE LAND")
-
--- in addition to ConExecute("UI_SelectByCategory "..
--- could also directly call UISelectionByCategory(expression, addToCurSel, inViewFrustum, nearestToMouse, mustBeIdle)
+-- (see below for example useage)
 
 function SquaredDist(pos1, pos2)
     return VDist2Sq(pos1[1], pos1[3], pos2[1], pos2[3])
 end
 
-function AddNearestAlreadyNotIn(candidates, selectionToAddTo)
+function AddNearestNotAlreadyIn(candidates, selectionToAddTo)
     local mousePos = GetMouseWorldPos()
     local nearestIndex = nil
     for i, unit in candidates do
@@ -26,43 +21,6 @@ function AddNearestAlreadyNotIn(candidates, selectionToAddTo)
         table.insert(selectionToAddTo, candidates[nearestIndex])
     end
 end
-
-function AddNearestByCategoryToSelection(spec)
-    local toSelect = GetSelectedUnits() or {}     
-    ConExecute("UI_SelectByCategory " .. spec)
-    local candidates = GetSelectedUnits() or {}
-    AddNearestAlreadyNotIn(candidates, toSelect)
-    SelectUnits(toSelect)
-end
-
---[[
-INFO: GetEconData() returns:{
-INFO:   energyConsumed=0,
-INFO:   energyProduced=0,
-INFO:   energyRequested=0,
-INFO:   massConsumed=0,
-INFO:   massProduced=0,
-INFO:   massRequested=0
-INFO: }
-INFO: GetCommandQueue() returns:{
-INFO:   {
-INFO:     position={ <metatable=table: 1A0E8DE8>
-INFO:       157.19039916992,
-INFO:       14.826637268066,
-INFO:       144.65634155273
-INFO:     },
-INFO:     type="Move"
-INFO:   },
-INFO:   {
-INFO:     position={ <metatable=table: 1A0E8DE8>
-INFO:       160.0930480957,
-INFO:       14.762691497803,
-INFO:       155.28187561035
-INFO:     },
-INFO:     type="AggressiveMove"
-INFO:   }
-INFO: }
-]]--
 
 local maxDist = 35
 local maxDistSquared = maxDist * maxDist
@@ -82,46 +40,14 @@ function IsNearlyIdle(unit)
     return toEndSquared < maxDistSquared
 end
 
--- UI_Lua import("/mods/SelectionHelper/helper.lua").SelectIdleOrNearlyIdleByCategory("MOBILE LAND")
--- UI_Lua import("/mods/SelectionHelper/helper.lua").SelectIdle_OrFailingThatNearlyIdle_ByCategory("ENGINEER")
-
-function SelectIdleOrNearlyIdleByCategory(spec)
-    ConExecute("UI_SelectByCategory " .. spec)
-    local candidates = GetSelectedUnits() or {}
-    local toSelect = {}
-    for _, unit in candidates do
-        if unit:IsIdle() or IsNearlyIdle(unit) then
-            if not import('/mods/SelectionHelper/flag_as_busy.lua').UnitIsFlaggedAsBusy(unit) then
-                table.insert(toSelect, unit)
-            end
-        end 
+function GetRallyPoint(unit)
+    local t = unit:GetCommandQueue()
+    if not t or not t[1] then return end
+    local command = t[table.getn(t)]
+    if command.type ~= "Move" and command.type ~= "AggressiveMove" then
+        return
     end
-    SelectUnits(toSelect)
-end
-
-function SelectIdle_OrFailingThatNearlyIdle_ByCategory(spec)
-    ConExecute("UI_SelectByCategory " .. spec)
-    local candidates = GetSelectedUnits() or {}
-    local toSelect = {}
-    for _, unit in candidates do
-        if unit:IsIdle() then
-            if not import('/mods/SelectionHelper/flag_as_busy.lua').UnitIsFlaggedAsBusy(unit) then
-                table.insert(toSelect, unit)
-            end
-        end 
-    end
-	if not toSelect[1] then
-		-- failed to find any idle
-		-- so now fall back on 'nearly idle', if any
-		for _, unit in candidates do
-			if IsNearlyIdle(unit) then
-				if not import('/mods/SelectionHelper/flag_as_busy.lua').UnitIsFlaggedAsBusy(unit) then
-					table.insert(toSelect, unit)
-				end
-			end 
-		end
-	end
-    SelectUnits(toSelect)
+    return command.position
 end
 
 function FilterDownToIdleOrNearlyIdle(candidates)
@@ -136,20 +62,146 @@ function FilterDownToIdleOrNearlyIdle(candidates)
     return filtered
 end
 
-function SelectIdleOrNearlyIdleByCategory(spec)
-    ConExecute("UI_SelectByCategory " .. spec)
+function FilterDownToIdle_OrFailingThatNearlyIdle(candidates)
+    local filtered = {}
+    for _, unit in candidates do
+        if unit:IsIdle() or IsNearlyIdle(unit) then
+            if not import('/mods/SelectionHelper/flag_as_busy.lua').UnitIsFlaggedAsBusy(unit) then
+                table.insert(filtered, unit)
+            end
+        end 
+    end
+	if not filtered[1] then
+		-- failed to find any idle
+		-- so now fall back on 'nearly idle', if any
+		for _, unit in candidates do
+			if IsNearlyIdle(unit) then
+				if not import('/mods/SelectionHelper/flag_as_busy.lua').UnitIsFlaggedAsBusy(unit) then
+					table.insert(toSelect, unit)
+				end
+			end 
+		end
+	end
+    return filtered
+end
+
+function FilterOut(candidates, exclude)
+    parsedExclude = ParseEntityCategory(exclude)
+    return EntityCategoryFilterOut(parsedExclude, candidates)
+end
+function FilterDown(candidates, include)
+    parsedInclude = ParseEntityCategory(include)
+    return EntityCategoryFilterDown(parsedInclude, candidates)
+end
+
+
+-- ** actual selection helper methods **
+
+-- call as follows:
+-- UI_Lua import("/mods/SelectionHelper/helper.lua").SelectIdleOrNearlyIdle('+inview ', 'MOBILE LAND', 'ENGINEER')
+-- UI_Lua import("/mods/SelectionHelper/helper.lua").FilterOrSelect('+inview ', 'MOBILE LAND', 'ENGINEER')
+
+-- note: in addition to ConExecute("UI_SelectByCategory "..whatever)
+-- could also directly call UISelectionByCategory(expression, addToCurSel, inViewFrustum, nearestToMouse, mustBeIdle)
+
+function SelectIdle_OrFailingThatNearlyIdle(selectionPrefix, include, exclude)
+    ConExecute("UI_SelectByCategory " .. selectionPrefix .. include)
     local candidates = GetSelectedUnits() or {}
+    candidates = FilterOut(candidates, exclude)
+    SelectUnits(FilterDownToIdle_OrFailingThatNearlyIdle(candidates))
+end
+
+function SelectIdleOrNearlyIdle(selectionPrefix, include, exclude)
+    ConExecute("UI_SelectByCategory " .. selectionPrefix .. include)
+    local candidates = GetSelectedUnits() or {}
+    candidates = FilterOut(candidates, exclude)
     SelectUnits(FilterDownToIdleOrNearlyIdle(candidates))
 end
 
-function SelectByCategory(spec)
-    ConExecute("UI_SelectByCategory " .. spec)
+function Select(selectionPrefix, include, exclude)
+    ConExecute("UI_SelectByCategory " .. selectionPrefix .. include)
+    local candidates = GetSelectedUnits() or {}
+    candidates = FilterOut(candidates, exclude)
+    SelectUnits(candidates)
 end
 
-function AddNearestIdleOrNearlyIdleByCategoryToSelection(spec)
-    local toSelect = GetSelectedUnits() or {}     
-    ConExecute("UI_SelectByCategory " .. spec)
+function FilterOrSelect_Idle_OrFailingThatNearlyIdle(selectionPrefix, include, exclude)
     local candidates = GetSelectedUnits() or {}
-    AddNearestAlreadyNotIn(FilterDownToIdleOrNearlyIdle(candidates), toSelect)
+    candidates = FilterDown(candidates, include)
+    candidates = FilterOut(candidates, exclude)
+    candidates = FilterDownToIdle_OrFailingThatNearlyIdle(candidates)
+    if not candidates[1] then
+        ConExecute("UI_SelectByCategory " .. selectionPrefix .. include)
+        candidates = GetSelectedUnits() or {}
+        candidates = FilterOut(candidates, exclude)
+    end
+    SelectUnits(FilterDownToIdle_OrFailingThatNearlyIdle(candidates))
+end
+
+function FilterOrSelect_IdleOrNearlyIdle(selectionPrefix, include, exclude)
+    local candidates = GetSelectedUnits() or {}
+    candidates = FilterDown(candidates, include)
+    candidates = FilterOut(candidates, exclude)
+    candidates = FilterDownToIdleOrNearlyIdle(candidates)
+    if not candidates[1] then
+        ConExecute("UI_SelectByCategory " .. selectionPrefix .. include)
+        candidates = GetSelectedUnits() or {}
+        candidates = FilterOut(candidates, exclude)
+    end
+    SelectUnits(FilterDownToIdleOrNearlyIdle(candidates))
+end
+
+function FilterOrSelect(selectionPrefix, include, exclude)
+    local candidates = GetSelectedUnits() or {}
+    candidates = FilterDown(candidates, include)
+    candidates = FilterOut(candidates, exclude)
+    if not candidates[1] then
+        ConExecute("UI_SelectByCategory " .. selectionPrefix .. include)
+        candidates = GetSelectedUnits() or {}
+        candidates = FilterOut(candidates, exclude)
+    end
+    SelectUnits(candidates)
+end
+
+function AddNearest(selectionPrefix, include, exclude)
+    local toSelect = GetSelectedUnits() or {}     
+    ConExecute("UI_SelectByCategory " .. selectionPrefix .. include)
+    local candidates = GetSelectedUnits() or {}
+    candidates = FilterOut(candidates, exclude)
+    AddNearestNotAlreadyIn(candidates, toSelect)
     SelectUnits(toSelect)
+end
+
+function AddNearestIdleOrNearlyIdle(selectionPrefix, include, exclude)
+    local toSelect = GetSelectedUnits() or {}     
+    ConExecute("UI_SelectByCategory " .. selectionPrefix .. include)
+    local candidates = GetSelectedUnits() or {}
+    candidates = FilterOut(candidates, exclude)
+    AddNearestNotAlreadyIn(FilterDownToIdleOrNearlyIdle(candidates), toSelect)
+    SelectUnits(toSelect)
+end
+
+-- UI_Lua import("/mods/SelectionHelper/helper.lua").SelectFactoryWithNearestRallyPoint()
+function SelectFactoryWithNearestRallyPoint()
+    ConExecute("UI_SelectByCategory FACTORY")
+    local candidates = GetSelectedUnits() or {}
+    if not candidates[1] then return end -- (no factories)
+    local best = nil
+    local bestDist
+    local mousePos = GetMouseWorldPos()
+    for _, unit in candidates do
+        local rallyPoint = GetRallyPoint(unit)
+        if rallyPoint then
+            local dist = SquaredDist(mousePos, rallyPoint)
+            if not best or dist < bestDist then
+                best = unit
+                bestDist = dist
+            end
+        end
+    end
+    if best then
+        SelectUnits({best})
+    else
+        SelectUnits({})
+    end
 end
