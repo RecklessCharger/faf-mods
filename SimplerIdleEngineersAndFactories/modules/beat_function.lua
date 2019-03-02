785a97c34c19ebda64eb829d0542129f5e56ee34
Thomas Young <thomas@upcoder.com>
2019-03-02 17:29:14 +0100

    fix for issues when view setup changes (e.g. switch to split screen) after idle engineer adornment is created

--- SimplerIdleEngineersAndFactories/modules/beat_function.lua
+++ SimplerIdleEngineersAndFactories/modules/beat_function.lua
@@ -8,19 +8,21 @@ function CreateOverlay(unit)
 	local overlay = Bitmap(GetFrame(0))
 	overlay.Width:Set(10)
 	overlay.Height:Set(10)
-	local worldView = import('/lua/ui/game/worldview.lua').viewLeft
+	local worldView = import('/lua/ui/game/worldview.lua')
 	overlay:SetNeedsFrameUpdate(true)
 	overlay.OnFrame = function(self, delta)
-		local pos = worldView:Project(unit:GetPosition())
-		LayoutHelpers.AtLeftTopIn(overlay, worldView, pos.x - overlay.Width() + 5, pos.y - overlay.Height() - 9)
+        local viewLeft = worldView.viewLeft
+		local pos = viewLeft:Project(unit:GetPosition())
+		LayoutHelpers.AtLeftTopIn(overlay, viewLeft, pos.x - overlay.Width() + 5, pos.y - overlay.Height() - 9)
 	end
     overlay.id = unit:GetEntityId()
 	overlay.text = UIUtil.CreateText(overlay, '<??>', 14, UIUtil.bodyFont)
 	overlay.text:SetColor('white')
 	overlay.text:SetDropShadow(true)
 	LayoutHelpers.AtCenterIn(overlay.text, overlay, 0, 0)
-	local pos = worldView:Project(unit:GetPosition())
-	LayoutHelpers.AtLeftTopIn(overlay, worldView, pos.x - overlay.Width() + 5, pos.y - overlay.Height() - 9)
+    local viewLeft = worldView.viewLeft
+	local pos = viewLeft:Project(unit:GetPosition())
+	LayoutHelpers.AtLeftTopIn(overlay, viewLeft, pos.x - overlay.Width() + 5, pos.y - overlay.Height() - 9)
 	return overlay
 end
 
